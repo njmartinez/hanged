b7333314be3ac03df37bb37e129a8a60ea2d602f
kleer <kleer@kleer-csd-box>
2017-06-14 11:54:53 -0500

    versionestable

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require './config'
+require './lib/hanged.rb'
 
 get '/' do
+	@letra = params["letra"]
 	erb :hanged
 end
--- spec/hanged_spec.rb
+++ spec/hanged_spec.rb
@@ -1,40 +1,40 @@
-require "./hanged.rb"
+require "./lib/hanged.rb"
 
 describe "Hanged" do 
 
 	it "selecciona letra C" do
-        ahorcado = Hanged.new
+        ahorcado = Hanged.new "C"
         resultado = ahorcado.comparar
         expect(resultado).to eq "S"
     end
 
     it "selecciona letra U" do
-        ahorcado = Hanged.new
+        ahorcado = Hanged.new "U"
         resultado = ahorcado.comparar
         expect(resultado).to eq "S"
     end
 
     it "selecciona letra M" do
-        ahorcado = Hanged.new
+        ahorcado = Hanged.new "M"
         resultado = ahorcado.comparar
         expect(resultado).to eq "S"
     end
 
     it "selecciona letra B" do
-        ahorcado = Hanged.new
+        ahorcado = Hanged.new "B"
         resultado = ahorcado.comparar
         expect(resultado).to eq "S"
     end
 
     it "selecciona letra I" do
-        ahorcado = Hanged.new
+        ahorcado = Hanged.new "I"
         resultado = ahorcado.comparar
         expect(resultado).to eq "S"
     end
   
-  
+
 	it "selecciona letra A" do
-        ahorcado = Hanged.new
+        ahorcado = Hanged.new "A"
         resultado = ahorcado.comparar
         expect(resultado).to eq "S"
     end
